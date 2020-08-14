41717aa67b50eaecf4565915d88bba145ab38608
victor <victor.palomar@live.com>
2020-08-13 23:39:38 -0700

    test

--- src/app/component/navigation-list/navigation-list.component.ts
+++ src/app/component/navigation-list/navigation-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navigation-list',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-list.component.scss']
 })
 export class NavigationListComponent implements OnInit {
+@Output() sidenavClose = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onSidenavClose = () => {
+  this.sidenavClose.emit();
+  }
 }
